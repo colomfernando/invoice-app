02cd328b82c41c6634171ee230eb2c3a5de19648
Fernando <colom_fernando@bitbucket.org>
2022-03-28 11:10:37 -0300

    feat: :tada: setup project
    install packages and config

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import GlobalStyles from 'theme/GlobalStyles';
+import Home from 'pages/index';
+import Invoice from 'pages/invoice';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const App: React.FC = () => {
   return (
-    <>
+    <BrowserRouter>
       <GlobalStyles />
-    </>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/invoice" element={<Invoice />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
---
+++ src/pages/invoice/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Invoice: React.FC = () => {
+  return <div>invoice detail</div>;
+};
+
+export default Invoice;
